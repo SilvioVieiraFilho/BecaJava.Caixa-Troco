3c332a40ca286e851534f0a17af8a8af6aea96a5
Silvio Rodrigues Vieira <srodrvie@everis.com>
2020-10-22 21:05:27 -0300

    first commit

---
+++ src/Caixa/CaixaEletronico.java
@@ -0,0 +1,45 @@
+package Caixa;
+
+import java.util.Scanner;
+
+public class CaixaEletronico {
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		int uninotas; // variavel unidade de quantas notass
+
+		System.out.println("Escreva o Valor do Saque:");
+
+		int valor = s.nextInt(); // recebe o valor do usuario
+
+		int vetor[] = new int[7]; // meu vetor que armazena minhas notas
+
+		vetor[0] = 200;
+		vetor[1] = 100;
+		vetor[2] = 50;
+		vetor[3] = 20;
+		vetor[4] = 10;
+		vetor[5] = 5;
+		vetor[6] = 2;
+
+		System.out.println("veja as opções");
+
+		for (int i = 1; i < vetor.length; i++) { // laço de repetição que o tamanho é o tamanho do vetor
+
+			if (valor >= vetor[i]) { // a condição é se o valor for maior que o vetor minha cédula.
+
+				uninotas = valor / vetor[i]; // valor divido por a nota mais proxima da entrada.
+
+				System.out.println("Saque " + uninotas + " nota de " + vetor[i]); // printo o valor das quantidades das
+																					// notas que precisara ser utilizado
+																					// e também informa quais sao as
+																					// notas
+
+			}
+
+		}
+
+	}
+
+}
